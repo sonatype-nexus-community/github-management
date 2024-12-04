e168440ac9fda0bce5afb920c36af2c8bbb1d6e5
Dan Rollo <drollo@sonatype.com>
2024-12-04 14:54:41 -0500

    Use GH Team in code owners file (#8)
    * use github team in owners file
    
    * Revert "fix: add exception handling and logging for #7"
    
    This reverts commit 48c1929e52ed541f8c811b1cdf05e1ab0e2bd241.
    
    * debugging stuff, no functional changes in this commit.

--- github_standards/standards.py
+++ github_standards/standards.py
@@ -79,11 +79,8 @@ def check_and_apply_standard_properties_to_branch(repo, branch: Branch, do_actua
     if branch_protection is None or props_not_as_per_standards != '':
         print(f'    Setting Standards for {repo.name} - missing {props_not_as_per_standards}')
         if do_actual_work:
-            try:
-                branch.edit_protection(**standard_branch_protection)
-                print(f'        Branch Standards applied')
-            except GithubException as e:
-                print(f'FAIL:   Failed to apply branch standards for {repo.name}')
+            branch.edit_protection(**standard_branch_protection)
+            print(f'        Branch Standards applied')
 
     standard_pull_request_reviews = {
         'require_code_owner_reviews': True,
